bc5f23b5d59ffcd00bebd1351b7c7b23e469e46e
Alvaro <alvarolukmanto@gmail.com>
2022-09-25 11:39:48 +0700

    feature - init react router

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useRef, useState } from "react"
+
+function HomePage () {
+  return (
+    <div>
+      <h1>
+        <span>STAR</span>
+        <span>WARS</span>
+      </h1>
+    </div>
+  )
+}
+
+export default HomePage
